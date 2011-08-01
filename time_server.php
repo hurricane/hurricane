af2b2b6bd63352c5fad3bb970fd7205ddbb1a3f5
Ilia Cheishvili <ilia.cheishvili@gmail.com>
2011-08-01 10:26:10 -0600

    removing extra logging

--- time_server.php
+++ time_server.php
@@ -7,7 +7,6 @@ date_default_timezone_set('America/Denver');
 $gateway = Erlang\Gateway::getInstance();
 while (true) {
     $message = $gateway->recv();
-    file_put_contents('/Users/icheishvili/Desktop/log.txt', print_r($message, true), FILE_APPEND);
     $message->data[0] = new Erlang\Atom('response');
     $message->data[3] = date('Y-m-d H:i:s');
     $gateway->send($message);
