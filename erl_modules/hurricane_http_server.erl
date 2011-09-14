eb36fc8665c3bbed00c06cb24ee5c834ebb2cc09
Ilia Cheishvili <ilia.cheishvili@gmail.com>
2011-09-13 23:27:38 -0600

    configurable handlers, timeouts, etc for http module

--- erl_modules/hurricane_http_server.erl
+++ erl_modules/hurricane_http_server.erl
@@ -7,28 +7,6 @@
 
 -export([start/1]).
 
-%% Receives an HTTP request, re-formats it into a friendlier data-
-%% structure, and passes it on to an http_handler process. The
-%% response, when received, is sent back to the client.
-http_handler(Request) ->
-    HandlerPid = hurricane_utils:get_best_pid(http_handler),
-    SendData = [
-        {scheme, Request:get(scheme)},
-        {version, Request:get(version)},
-        {method, Request:get(method)},
-        {headers, mochiweb_headers:to_list(Request:get(headers))},
-        {raw_path, Request:get(raw_path)},
-        {path, Request:get(path)},
-        {params, Request:parse_qs()},
-        {body, Request:recv_body(Request:get(body_length))}
-    ],
-    HandlerPid ! {request, erlang:self(), http_request, SendData},
-    receive
-        {response, _FromPid, http_request, Data} -> Request:respond(Data)
-    after 10000 ->
-        Request:respond({504, [], "Internal Timeout."})
-    end.
-
 %% Sets up an infinite loop for the managing process so that it never
 %% has to exit (and thus be restarted by the supervisor).
 loop() ->
@@ -43,10 +21,33 @@ loop() ->
 start(Options) ->
     application:start(mochiweb),
     ListenPort = proplists:get_value(listen_port, Options, 80),
+    HandlerGroup = proplists:get_value(handler_group, Options, http_handler),
+    ResponseTimeout = proplists:get_value(response_timeout, Options, 10000),
+
+    HttpHandler = fun(Request) ->
+        HandlerPid = hurricane_utils:get_best_pid(HandlerGroup),
+        SendData = [
+            {scheme, Request:get(scheme)},
+            {version, Request:get(version)},
+            {method, Request:get(method)},
+            {headers, mochiweb_headers:to_list(Request:get(headers))},
+            {raw_path, Request:get(raw_path)},
+            {path, Request:get(path)},
+            {params, Request:parse_qs()},
+            {body, Request:recv_body(Request:get(body_length))}
+        ],
+        HandlerPid ! {request, erlang:self(), http_request, SendData},
+        receive
+            {response, _FromPid, http_request, Data} -> Request:respond(Data)
+        after ResponseTimeout ->
+            Request:respond({504, [], "Internal Timeout."})
+        end
+    end,
+
     mochiweb_http:start(
         [
             {name, proplists:get_value(name, Options, default)},
-            {loop, fun http_handler/1},
+            {loop, HttpHandler},
             {port, ListenPort}
         ]
     ),
