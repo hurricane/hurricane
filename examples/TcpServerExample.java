82754694246410d92186e6d8b6d7d0945a52dcc0
Ilia Cheishvili <ilia.cheishvili@gmail.com>
2011-09-28 23:30:10 -0600

    java server example

---
+++ examples/TcpServerExample.java
@@ -0,0 +1,28 @@
+import org.hurricane.driver.Gateway;
+import org.hurricane.driver.SocketWrapper;
+import org.hurricane.driver.datatypes.Atom;
+import org.hurricane.driver.datatypes.Tuple;
+import org.hurricane.driver.datatypes.Nil;
+import java.util.Date;
+
+public class TcpServerExample {
+    public static void main(String[] args) throws Exception {
+        Gateway gateway = new Gateway("localhost", 3307);
+        Tuple regMsg = new Tuple();
+        regMsg.elements().add(new Atom("register_with_group"));
+        regMsg.elements().add(new Atom("time_server"));
+        gateway.send(regMsg);
+
+        while (true) {
+            Tuple recvMsg = (Tuple) gateway.recv();
+            if (recvMsg.elements().get(0).equals(new Atom("request"))) {
+                Tuple sendMsg = new Tuple();
+                sendMsg.elements().add(new Atom("response"));
+                sendMsg.elements().add(recvMsg.elements().get(1));
+                sendMsg.elements().add(recvMsg.elements().get(2));
+                sendMsg.elements().add(new Date().toString());
+                gateway.send(sendMsg);
+            }
+        }
+    }
+}
