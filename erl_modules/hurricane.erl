9a74f66761f8017d1a644a4b6d7416ee26fc5b02
Ilia Cheishvili <ilia.cheishvili@gmail.com>
2012-02-25 15:01:27 -0700

    made config server a gen server

--- erl_modules/hurricane.erl
+++ erl_modules/hurricane.erl
@@ -9,6 +9,6 @@
 %% defined in the config.
 start(Options) ->
     application:start(sasl),
-    erlang:spawn_link(hurricane_config_server, start, [Options]),
-    erlang:spawn_link(hurricane_log_server, start, [[]]),
+    hurricane_config_server:start(Options),
+    hurricane_log_server:start([]),
     erlang:spawn_link(hurricane_supervisor, start, [[]]).
--- erl_modules/hurricane_config_server.erl
+++ erl_modules/hurricane_config_server.erl
@@ -4,42 +4,55 @@
 
 -module(hurricane_config_server).
 
--export([get_config/1, get_config/2, start/1]).
+-behaviour(gen_server).
 
-%% Runs forever. Supports reloading the config and serving config
-%% values up to any process that asks for them.
-loop(State) ->
-    receive
-        {_From, reload_config} ->
-            ConfigPath = proplists:get_value(config_path, State),
-            LoadConfigFun = proplists:get_value(load_config_fun, State),
-            StateNoConfig = lists:keydelete(config, 1, State),
-            Config = erlang:apply(LoadConfigFun, [ConfigPath]),
-            NewState = [{config, Config} | StateNoConfig];
-        {From, get_config, Key} ->
-            Config = proplists:get_value(config, State, []),
-            Value = proplists:get_value(Key, Config),
-            From ! {config_for, Key, Value},
-            NewState = State;
-        Other ->
-            io:format(
-                standard_error,
-                "config server received unknown message ~p~n",
-                [Other]
-            ),
-            NewState = State
-    end,
-    loop(NewState).
+-export([get_config/1, get_config/2, start/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%% Called to initialize the process.
+init(Args) ->
+    {ok, Args}.
+
+%% Used to requests for configuration.
+handle_call({get_config, Key}, _From, State) ->
+    Config = proplists:get_value(config, State, []),
+    Value = proplists:get_value(Key, Config, undefined),
+    {reply, Value, State};
+
+%% Handle the reload config message.
+handle_call({reload_config}, _From, State) ->
+    ConfigPath = proplists:get_value(config_path, State),
+    LoadConfigFun = proplists:get_value(load_config_fun, State),
+    StateNoConfig = lists:keydelete(config, 1, State),
+    Config = erlang:apply(LoadConfigFun, [ConfigPath]),
+    NewState = [{config, Config} | StateNoConfig],
+    {reply, ok, NewState};
+
+%% Used to handle direct, synchronous requests.
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+%% Handle all other asynchronous messages.
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+%% Used to handle direct process messages.
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% Called before exiting--clean up happens here.
+terminate(_Reason, _State) ->
+    ok.
+
+%% Called to upgrade the currently running code.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %% Provides a convenient way for other processes to get the config from
 %% the config server (encapsulates the messaging that has to happen and
 %% how to receive a reply).
 get_config(Key) ->
-    ?MODULE ! {erlang:self(), get_config, Key},
-    receive
-        {config_for, Key, Value} -> ok
-    end,
-    Value.
+    gen_server:call(?MODULE, {get_config, Key}).
 
 %% Provides a convenient way for other processes to get the config from
 %% the config server (encapsulates the messaging that has to happen and
@@ -53,6 +66,5 @@ get_config(Key, Default) ->
 %% Registers the process under a convenience name, send a message to
 %% load the config, and enters the serve loop.
 start(State) ->
-    erlang:register(?MODULE, erlang:self()),
-    erlang:self() ! {erlang:self(), reload_config},
-    loop(State).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, State, []),
+    gen_server:call(?MODULE, {reload_config}).
